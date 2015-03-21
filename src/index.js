199f95635a4f255aa85df5ba65bd673417b7c9f7
Cami Rodriguez <camiepisode@outlook.com>
2015-03-20 21:08:14 -0500

    Adjust to Python's tuples

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ io.sockets.on('connection', function(socket){
 
         /// Command from Koh.js (WinJS) to Hydra (Python)
         socket.on('command.KohJS.Hy', function(data){
-                io.sockets.emit('command.Hydra', {Command: data.Command, Values:  data.Values });
+                io.sockets.emit('command.Hydra', JSON.stringify({Command: data.Command, Values:  data.Values }));
         });
 
 
